4aff0c378b954b0f2e2b6704927ee18975d26fc7
Yu Hong <hello.hyu@gmail.com>
2012-12-21 11:58:06 +0800

    restaurant

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
 	$('.carousel').carousel();
+	$('.smlHoverdown').on('mouseenter', function(e){
+		var link = $(e.currentTarget);
+		link.addClass('open');
+	}).on('mouseleave', function(e){
+		var link = $(e.currentTarget);
+		link.removeClass('open');
+	});
 });
