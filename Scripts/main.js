c6cd6bf160c3c810645d43ed680e0de9fa5f6673
Yu Hong <hello.hyu@gmail.com>
2012-12-24 20:07:17 +0800

    custom lunchbox

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,3 +1,25 @@
+function addToLunchbox(courseJSON){
+	var list = $('#lunchbox-'+courseJSON.type);
+	console.log(list.size());
+	if ( courseJSON.type == 'entrees' || courseJSON.type == 'sides' ) {
+		emptyItem = list.find('.lunchbox-emptyItem');
+		console.log(emptyItem.size());
+		if ( emptyItem.size() > 0 ) {
+		
+			$(emptyItem[0]).html( '<i class="icon-remove icon-brown" title="Remove" rel="tooltip"></i> ' + courseJSON.name).removeClass('lunchbox-emptyItem').addClass('lunchbox-filledItem');
+			
+			return true;
+			
+		} else {
+			alert('no empty item');
+			return false;
+		}
+	}
+	if ( courseJSON.type == 'others' ) {
+		list.append( '<li class="lunchbox-filledItem"><i class="icon-remove icon-brown" title="Remove" rel="tooltip"></i> ' + courseJSON.name + '</li>').removeClass('lunchbox-emptyItem').addClass('lunchbox-filledItem');
+	}
+	return false;
+}
 $(document).ready(function(){
 	$('.carousel').carousel();
 	$('.smlHoverdown').on('mouseenter', function(e){
@@ -13,10 +35,65 @@ $(document).ready(function(){
 	}).on('mouseleave', function(e){
 		var item = $(e.currentTarget);
 		item.find('.lunchbox-select').removeClass('btn');
-	}).onc('click',function(e){
+	}).on('click',function(e){
 		var item = $(e.currentTarget);
 		item.find('.lunchbox-select').trigger('click');
 
 	});
+	
+	$('[rel="tooltip"]').tooltip();
+	
+	$('.course').on('click', function(e){
+		$(e.currentTarget).toggleClass('selected');
+		
+		if ($(e.currentTarget).hasClass('selected')) {
+		var courseImg = $(e.currentTarget).find('img');
+		var lunchbox = $('.lunchboxBody');
+		
+		var productX 		= courseImg.offset().left;
+		var productY 		= courseImg.offset().top;
+		
+		
+		var basketX 		= lunchbox.offset().left;
+		var basketY 		= lunchbox.offset().top;
+		
+		
+		var gotoX 			= basketX - productX;
+		var gotoY 			= basketY - productY;
+		
+		var newImageWidth 	= courseImg.width() / 3;
+		var newImageHeight	= courseImg.height() / 3;
+		
+		courseImg
+		.clone()
+		.prependTo($(e.currentTarget))
+		.css({'position' : 'absolute','z-index':99,})
+		.animate({opacity: 0.4}, 100 )
+		.animate({opacity: 0.1, marginLeft: gotoX, marginTop: gotoY, width: newImageWidth, height: newImageHeight}, 1200, function() {
+			
+			$(this).remove();
+		
+		
+			//success
+			addToLunchbox({type:'entrees',name:'Kung Pao Chicken'});
+			
+			$.ajax({  
+				type: "POST",  
+				url: "inc/functions.php",  
+				data: { productID: 'nothing', action: "addToBasket"},  
+				success: function(theResponse) {
+					
+					
+					
+				}  
+			});  
+		
+		});
+	}//end if
+
+	});
+	
+	
+
 
 });
